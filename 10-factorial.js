fe8f96759602586dd7d22d48612342563e584ad3
Cintoma123 <calistusumehnnachebe@gmail.com>
2025-09-09 05:01:49 +0100

    updated my factotial

--- 10-factorial.js
+++ 10-factorial.js
@@ -3,7 +3,7 @@ function factorialRecursive(n) {
     if (n === 0) return 1;
     return n * factorialRecursive(n - 1);
 }
-console.log(factorialRecursive(1));
+console.log(factorialRecursive(0));
 console.log(factorialRecursive(3));
 console.log(factorialRecursive(89));
 console.log(factorialRecursive(333));
